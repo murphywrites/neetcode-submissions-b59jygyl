cea944602df1dbfaa6215cc2c63025fe9f26082d
Michael Murphy <mike@murphyscode.com>
2026-04-09 15:56:10 -0700

    Add: valid-binary-search-tree - submission-6

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-6.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+
+    isValidBST(root) {
+        
+        const validate = (root, min, max) => {
+            if(root === null) return true
+            // if(root.left === null && root.right === null) return true
+            let leftValid = true
+            let rightValid = true
+            if(root.val > min && root.val < max) {
+                    leftValid = validate(root.left,min,root.val)
+                    rightValid = validate(root.right,root.val,max)
+                console.log(rightValid,leftValid)
+                return leftValid && rightValid
+            } else {
+                return false
+            }
+        
+        
+    }
+
+    return validate(root, -Infinity, Infinity)
+
+}}
